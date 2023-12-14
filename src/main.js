3c0f23a58142f73584f2c58b5711ed3ef5865301
Mcrivers0517 <wangrhofficial@gmail.com>
2023-12-14 18:17:58 +0800

    留给后端的接口还没写

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd);
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
