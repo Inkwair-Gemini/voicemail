cbbb14382c1247c7988ede1b368255f5b4cc6025
Mcrivers0517 <wangrhofficial@gmail.com>
2023-12-27 14:30:00 +0800

    增加了vuex依赖项

--- src/main.js
+++ src/main.js
@@ -5,21 +5,40 @@ import App from './App.vue'
 import { DatePicker } from "ant-design-vue";
 import Antd from "ant-design-vue"
 import 'ant-design-vue/dist/antd.css';
-//引入路由
-import VueRouter from 'vue-router'
-//引入路由器
-import router from './router'
-//引入国际化
-import VueI18n from 'vue-i18n'
+import i18n from './lang'
+import VueRouter from 'vue-router' //引入路由
+import router from './router' //引入路由器
+import VueI18n from 'vue-i18n' //引入国际化
+import Vuex from 'vuex' //引入VueX
+
+Vue.use(Vuex)
 Vue.use(VueI18n)
-Vue.use(ElementUI);
+Vue.use(ElementUI)
 Vue.use(VueRouter)
-Vue.use(Antd);
-Vue.use(DatePicker);
+Vue.use(Antd)
+Vue.use(DatePicker)
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  state: {
+    lang: 'zh-CN'
+  },
+  mutations: {
+    changeLang(state, lang) {
+      state.lang = lang
+    },
+  },
+  actions: {
+    setLang(context, lang) {
+      context.commit('changeLang', lang)
+    },
+  }
+})
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  i18n,
+  store,
 }).$mount('#app')
 
